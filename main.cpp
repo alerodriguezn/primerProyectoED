736ffa07827a47b56a9c5ef578c6843f3f6f31b7
DeividMatuteG <deividmg02@gmail.com>
2022-09-25 20:43:05 -0600

    Se agrego funciones con fechas y horas

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <ctime> //Libreria utilizada para el manejo de las fechas
+#include <vector>  //Libreria utilizada para el manejo de las fechas.
 
 using namespace std;
+
+//Se crea un vector que almacenara los meses
+vector<string> mes;
+void llenarVectorMeses(){
+    mes.push_back("Enero");
+    mes.push_back("Febrero");
+    mes.push_back("Marzo");
+    mes.push_back("Abril");
+    mes.push_back("Mayo");
+    mes.push_back("Junio");
+    mes.push_back("Julio");
+    mes.push_back("Agosto");
+    mes.push_back("Septiembre");
+    mes.push_back("Octubre");
+    mes.push_back("Noviembre");
+    mes.push_back("Diciembre");
+}
 
 /*  ESTRUCTURAS   */
 struct personas{
@@ -29,6 +48,7 @@ struct personas{
     }
 }*listaPersonas;
 
+
 struct lluvia{
     string codigo;
     string nombre;
@@ -53,13 +73,13 @@ struct region{
 
     region(int id, string nombre, string ubicacion){
         this->id = id;
-        this->nombre = nombre; 
+        this->nombre = nombre;
         this->ubicacion = ubicacion;
         sig = NULL;
     }
 
     void agregarLugar(struct lugar*nuevoLugar){
-        //Aqui iria la funcion de agregar lugar 
+        //Aqui iria la funcion de agregar lugar
         cout << "Agregando nuevo lugar";
     }
 } *listaRegion;
@@ -83,19 +103,19 @@ struct lugar{
         //Aquí iría el metodo de agregar un tiempo a la sublista de tiempos o directamente mandar una lista de tiempos
         cout << "Agregando nuevo registro de tiempo";
     }
-    
+
 } *listaLugar;
 
 struct efimeridad{
     string nombre;
-    string fecha;
-    string horaSalida;
-    string horaOcultamiento;
+    tm * fecha;
+    tm * horaSalida;
+    tm * horaOcultamiento;
 
     efimeridad* sig;
     efimeridad* ant;
 
-    efimeridad(string nombre, string fecha, string horaSalida, string horaOcultamiento){
+    efimeridad(string nombre, tm * fecha, tm * horaSalida, tm * horaOcultamiento){
         this->nombre = nombre;
         this->fecha = fecha;
         this->horaSalida = horaSalida;
@@ -103,7 +123,8 @@ struct efimeridad{
         sig = NULL;
         ant = NULL;
     }
-};
+}*listaEfimeridades;
+
 
 struct  tiempo
 {
@@ -182,7 +203,7 @@ personas *insertarPersona(string nombre, int cedula, string lugarResidencia, str
     }
 
     else if (nombre[0] < listaPersonas->nombre[0])
-    {               
+    {
         listaPersonas->ant = nuevaPersona;
         listaPersonas = nuevaPersona;
     }
@@ -202,9 +223,149 @@ personas *insertarPersona(string nombre, int cedula, string lugarResidencia, str
             nuevaPersona->sig = temp;
             temp->ant = nuevaPersona;
         }
-        return listaPersonas;
-        
     }
+    return listaPersonas;
+}
+
+
+//Inserta la efimeridad ordenada por fecha
+efimeridad *insertarEfimeridad(string nombre, tm * fecha, tm * horaSalida, tm * horaOcultamiento){
+
+    efimeridad * nuevaEfimeridad = new efimeridad(nombre, fecha, horaSalida, horaOcultamiento);
+
+    if (listaEfimeridades == NULL)
+    {
+        listaEfimeridades = nuevaEfimeridad;
+    }
+
+    else if ((fecha->tm_year <= listaEfimeridades->fecha->tm_year) && (fecha -> tm_mon <= listaEfimeridades->fecha->tm_mon) && (fecha->tm_mday <= listaEfimeridades->fecha->tm_mday)){
+        nuevaEfimeridad->sig = listaEfimeridades;
+        listaEfimeridades->ant = nuevaEfimeridad;
+        listaEfimeridades = nuevaEfimeridad;
+    }
+    else{
+        efimeridad* temp = listaEfimeridades;
+        efimeridad* te = listaEfimeridades;
+
+    /*
+    Analisis del siguiente while
+    Datos del Elemento nuevo:          x1=2022 x2=12 x3=16
+    Datos del Elemento en la lista:    y1=2022 y2=11 y3=17
+
+    Así se representaría para validar que sea mayor :
+    (   (x1>y1) || (  (x1 == y1) && (x2 > y2)  ) || (  (x1 == y1) && (x2 == y2) && (x3 > y3)  )  )
+    */
+        while ( (fecha->tm_year > listaEfimeridades->fecha->tm_year) || ( (fecha->tm_year == listaEfimeridades->fecha->tm_year) && (fecha->tm_mon > listaEfimeridades->fecha->tm_mon) ) || ( (fecha->tm_year == listaEfimeridades->fecha->tm_year) && (fecha->tm_mon == listaEfimeridades->fecha->tm_mon) && ((fecha->tm_mday > listaEfimeridades->fecha->tm_mday)) ))
+        {
+            te = temp;
+            if (temp->sig != NULL){
+                temp = temp->sig;
+            }
+            if(temp->sig == NULL){
+                cout << "teminamos la vara con final de " << temp->nombre << " y el nuevo seria " << nuevaEfimeridad->nombre;
+                temp->sig = nuevaEfimeridad;
+                nuevaEfimeridad->ant = temp;
+                break;
+            }
+
+        }
+        // nuevaEfimeridad->ant = te;
+        // te->sig = nuevaEfimeridad;
+        // if (temp != NULL)
+        // {
+        //     nuevaEfimeridad->sig = temp;
+        //     temp->ant = nuevaEfimeridad;
+        // }
+    }
+    return listaEfimeridades;
+}
+
+// Se encarga de pedir una fecha y la valida. Ademas crea y retorna una nueva instancia de tipo tm para manipular la fecha mas facil.
+
+tm * pedirFecha(){
+    int dia, mes, agno;
+    while (true){
+        cout << "\nDia del mes: ";
+        cin >> dia;
+        if((dia > 0)&&(dia<=31)){
+            break;
+        }
+        cout << "\nError: El dia del mes debe ser un numero del 1 al 31\n";
+    }
+    while (true){
+        cout << "\nMes: ";
+        cin >> mes;
+        if((mes > 0)&&(mes)<=12){
+            mes --; //Para la instancia de tm se necesitara dar el numero desde 0 a 11
+            break;
+        }
+        cout << "\nError: El mes debe ser un numero del 1 al 12\n";
+    }
+    while (true){
+        cout << "\nAgno: ";
+        cin>>agno;
+        if(to_string(agno).length()==4)
+            break;
+        cout << "\nError: El año debe tener 4 digitos. Ej: 2022\n";
+    }
+    tm * nuevaFecha = new tm();
+    nuevaFecha->tm_mday = dia;
+    nuevaFecha->tm_mon = mes;
+    nuevaFecha->tm_year = agno;
+
+    return nuevaFecha;
+}
+
+// Se encarga de pedir una hora y la valida. Ademas crea y retorna una nueva instancia de tipo tm para manipular la hora mas facil.
+
+tm * pedirHora(){
+    int hora, minutos, abreviatura;
+    while (true){
+        cout << "\nHora: ";
+        cin >> hora;
+        if((hora > 0)&&(hora<=12))
+            break;
+        cout << "\nError: La hora debe ser un numero del 1 al 12 unicamente\n";
+    }
+    while (true){
+        cout << "\nMinutos: ";
+        cin >> minutos;
+        if((minutos >= 0)&&(minutos<=59))
+            break;
+        cout << "\nError: Los minutos deben ser un numero del 00 al 59\n";
+    }
+    while (true){
+        cout << "\nSelecciona la abreviatura correcta: \n1. am\n2. pm\n-> ";
+        cin >> abreviatura;
+        if((abreviatura==1)||(abreviatura==2)){
+            if(abreviatura==2)
+                hora += 12; //La convierte en formato de 24 horas para el objeto tm.
+            break;
+        }
+        cout << "\nError: Debes seleccionar la opcion 1 o 2";
+    }
+
+    tm * nuevaHora = new tm();
+    nuevaHora->tm_hour = hora;
+    nuevaHora->tm_min = minutos;
+
+    return nuevaHora;
+}
+
+// Devuelve la fecha de la instancia tm dada en los parametros. La retorna así: "25 de Septiembre del 2022"
+string devolverFecha(tm* fecha){
+    return to_string(fecha->tm_mday) + " de " + mes[fecha->tm_mon] + " del " + to_string(fecha->tm_year);
+}
+
+// Devuelve la hora de la instancia tm dada en los parametros. La retorna así: "4:00 p.m"
+string devolverHora(tm* hora){
+    string minutos = to_string(hora->tm_min);
+    if (minutos.length() == 1)
+        minutos="0"+minutos;
+    if (hora->tm_hour>12)
+        return to_string(hora->tm_hour-12) + ":" + minutos + " p.m";
+    else
+        return to_string(hora->tm_hour) + ":" + minutos + " a.m";
 }
 
 /*IMPRIMIR DE CADA UNO DE LOS INSERTAR*/
@@ -271,44 +432,153 @@ void imprimirPersonas(personas* lista){
 
 }
 
+void imprimirEfimeridades(efimeridad* lista){
+    if(lista == NULL)
+        cout << "\nLA LISTA ESTA VACIA\n";
+    else{
+        efimeridad * temp = lista;
+        cout << "\nIMPRIMIENTO HACIA ADELANTE... \n";
+        while(temp->sig!=NULL){
+            cout<<"Nombre: "<<temp->nombre<<endl;
+            cout<<"Fecha: "<<devolverFecha(temp->fecha)<<endl;
+            cout<<"Hora Salida: "<<devolverHora(temp->horaSalida)<<endl;
+            cout<<"Hora Ocultamiento: "<<devolverHora(temp->horaOcultamiento)<<endl;
+            temp = temp->sig;
+        }
+        cout<<"Nombre: "<<temp->nombre<<endl;
+        cout<<"Fecha: "<<devolverFecha(temp->fecha)<<endl;
+        cout<<"Hora Salida: "<<devolverHora(temp->horaSalida)<<endl;
+        cout<<"Hora Ocultamiento: "<<devolverHora(temp->horaOcultamiento)<<endl;
+        //AHORA IMPRIMO HACIA ATRAS
+        cout << "\nIMPRIMIENTO HACIA ATRAS... \n";
+        while(temp!=NULL){
+            cout<<"Nombre: "<<temp->nombre<<endl;
+            cout<<"Fecha: "<<devolverFecha(temp->fecha)<<endl;
+            cout<<"Hora Salida: "<<devolverHora(temp->horaSalida)<<endl;
+            cout<<"Hora Ocultamiento: "<<devolverHora(temp->horaOcultamiento)<<endl;
+            temp = temp->ant;
+        }
+    }
+
+}
+
 
 
 /*  OTROS MÉTODOS   */
 void cargarDatos()
 {
-    listaLluvia = insertarLluvia("COD#1", "NombreL#1", 01, listaLluvia);
-    listaLluvia = insertarLluvia("COD#2", "NombreL#2", 02, listaLluvia);
-    listaLluvia = insertarLluvia("COD#3", "NombreL#3", 03, listaLluvia);
-    listaLluvia = insertarLluvia("COD#4", "NombreL#4", 04, listaLluvia);
+    // listaLluvia = insertarLluvia("COD#1", "NombreL#1", 01, listaLluvia);
+    // listaLluvia = insertarLluvia("COD#2", "NombreL#2", 02, listaLluvia);
+    // listaLluvia = insertarLluvia("COD#3", "NombreL#3", 03, listaLluvia);
+    // listaLluvia = insertarLluvia("COD#4", "NombreL#4", 04, listaLluvia);
 
-    listaRegion = insertarRegion(01, "Region#1", "Ubicaicacio1", listaRegion);
-    listaRegion = insertarRegion(02, "Region#2", "Ubicaicacio1", listaRegion);
-    listaRegion = insertarRegion(03, "Region#3", "Ubicaicacio1", listaRegion);
-    listaRegion = insertarRegion(04, "Region#4", "Ubicaicacio1", listaRegion);
+    // listaRegion = insertarRegion(01, "Region#1", "Ubicaicacio1", listaRegion);
+    // listaRegion = insertarRegion(02, "Region#2", "Ubicaicacio1", listaRegion);
+    // listaRegion = insertarRegion(03, "Region#3", "Ubicaicacio1", listaRegion);
+    // listaRegion = insertarRegion(04, "Region#4", "Ubicaicacio1", listaRegion);
 
-    listaLugar = insertarLugar("Lugar#1", 100, 2.5, listaLugar);
-    listaLugar = insertarLugar("Lugar#2", 100, 2.5, listaLugar);
-    listaLugar = insertarLugar("Lugar#3", 100, 2.5, listaLugar);
-    listaLugar = insertarLugar("Lugar#4", 100, 2.5, listaLugar);
+    // listaLugar = insertarLugar("Lugar#1", 100, 2.5, listaLugar);
+    // listaLugar = insertarLugar("Lugar#2", 100, 2.5, listaLugar);
+    // listaLugar = insertarLugar("Lugar#3", 100, 2.5, listaLugar);
+    // listaLugar = insertarLugar("Lugar#4", 100, 2.5, listaLugar);
 
 
-    listaPersonas = insertarPersona("Alejandro Rodriguez", 208360735, "El Tanque","2018");
-    listaPersonas = insertarPersona("Juan Ca", 2081230735, "La Fortuna","2013");
-    listaPersonas = insertarPersona("Beto Beto", 1011230735, "San Rafael","2022");
-    listaPersonas = insertarPersona("Roberto Jimenez", 3011230735, "Florencia","2021");
-    listaPersonas = insertarPersona("Alondra", 4011230735, "Santa Clara","2024");
-    listaPersonas = insertarPersona("Carlos Ruiz", 3011230735, "Florencia","2021");
-    listaPersonas = insertarPersona("Bianca Ruiz", 4011230735, "Florencia","2022");
+    // listaPersonas = insertarPersona("Alejandro Rodriguez", 208360735, "El Tanque","2018");
+    // listaPersonas = insertarPersona("Juan Ca", 2081230735, "La Fortuna","2013");
+    // listaPersonas = insertarPersona("Beto Beto", 1011230735, "San Rafael","2022");
+    // listaPersonas = insertarPersona("Roberto Jimenez", 3011230735, "Florencia","2021");
+    // listaPersonas = insertarPersona("Alondra", 4011230735, "Santa Clara","2024");
+    // listaPersonas = insertarPersona("Carlos Ruiz", 3011230735, "Florencia","2021");
+    // listaPersonas = insertarPersona("Bianca Ruiz", 4011230735, "Florencia","2022");
+
+
+    //Forma para pedir la fecha y hora en variables y insertarlas despues
+    // cout << "\nIngresa la fecha\n";
+    // tm * fecha1 = pedirFecha();
+    // cout << "\nIngresa la hora de salida\n";
+    // tm * hora1 = pedirHora();
+    // cout << "\nIngresa la hora de ocultamiento\n";
+    // tm * hora2 = pedirHora();
+
+    // cout << "\nIngresa la fecha\n";
+    // tm * fecha2 = pedirFecha();
+    // cout << "\nIngresa la hora de salida\n";
+    // tm * hora3 = pedirHora();
+    // cout << "\nIngresa la hora de ocultamiento\n";
+    // tm * hora4 = pedirHora();
+
+    // listaEfimeridades = insertarEfimeridad("Efimeridad 1",fecha1, hora1, hora2);
+    // listaEfimeridades = insertarEfimeridad("Efimeridad 2",fecha2, hora3, hora4);
+
+    //Otra forma de meterlos pero sin pedir los datos al usuario
+
+    // cout << "\nIngresa la fecha\n";
+    tm * fecha1 = new tm();
+    fecha1->tm_year = 2020;
+    fecha1->tm_mon = 3;
+    fecha1->tm_mday = 16;
+
+    // cout << "\nIngresa la hora de salida\n";
+    tm * hora1 = new tm();
+    hora1->tm_hour = 7;
+    hora1->tm_min = 25;
+
+    // cout << "\nIngresa la hora de ocultamiento\n";
+    tm * hora2 = new tm();
+    hora2->tm_hour = 17;
+    hora2->tm_min = 9;
+//---------------------------
+    // cout << "\nIngresa la fecha\n";
+    tm * fecha2 = new tm();
+    fecha2->tm_year = 2020;
+    fecha2->tm_mon = 2;
+    fecha2->tm_mday = 12;
+
+    // cout << "\nIngresa la hora de salida\n";
+    tm * hora3 = new tm();
+    hora3->tm_hour = 7;
+    hora3->tm_min = 25;
+
+    // cout << "\nIngresa la hora de ocultamiento\n";
+    tm * hora4 = new tm();
+    hora4->tm_hour = 17;
+    hora4->tm_min = 20;
+//---------------------------
+    // cout << "\nIngresa la fecha\n";
+    tm * fechaX = new tm();
+    fechaX->tm_year = 2020;
+    fechaX->tm_mon = 6;
+    fechaX->tm_mday = 11;
+
+    // cout << "\nIngresa la hora de salida\n";
+    tm * horaX = new tm();
+    horaX->tm_hour = 7;
+    horaX->tm_min = 25;
+
+    // cout << "\nIngresa la hora de ocultamiento\n";
+    tm * horaX2 = new tm();
+    horaX2->tm_hour = 12;
+    horaX2->tm_hour = 20;
+
+    listaEfimeridades = insertarEfimeridad("Efimeridad 1",fecha1, hora1, hora2);
+    listaEfimeridades = insertarEfimeridad("Efimeridad 2",fecha2, hora3, hora4);
+    listaEfimeridades = insertarEfimeridad("Efimeridad 3",fechaX, horaX, horaX2);
+
 }
+
 
 int main()
 {
+    llenarVectorMeses();
     cargarDatos();
+    imprimirEfimeridades(listaEfimeridades);
 
-    imprimirLluvia(listaLluvia);
-    imprimirRegion(listaRegion);
-    imprimirLugar(listaLugar);
-    imprimirPersonas(listaPersonas);
+
+    // imprimirLluvia(listaLluvia);
+    // imprimirRegion(listaRegion);
+    // imprimirLugar(listaLugar);
+    // imprimirPersonas(listaPersonas);
+
 
     return 0;
 }
